d873bcd02a1b8ca380cada9df60aff3617606802
Charles Varin <cvarin@gmail.com>
2012-03-26 17:01:40 -0400

    Modified parse.cpp to use the class IniParser (in construction).

--- test/parse.cpp
+++ test/parse.cpp
@@ -7,18 +7,15 @@
 
 int main(int argc, char * argv[])
 {
-    dictionary * ini ;
-    char       * ini_name ;
-
+    char *ini_name;
     if (argc<2) {
         ini_name = "twisted.ini";
     } else {
         ini_name = argv[1] ;
     }
 
-    ini = iniparser_load(ini_name);
-    iniparser_dump(ini, stdout);
-    iniparser_freedict(ini);
+    IniParser ini(ini_name);
+    iniparser_dump(ini.dic, stdout);
 
     return 0 ;
 }
